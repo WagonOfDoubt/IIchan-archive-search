9659e1b2276df28a96bab54a55f5172d4b3d6492
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2017-07-09 16:16:48 +0300

    :arrow_up: bump version

--- src/IIchan-archive-search.meta.js
+++ src/IIchan-archive-search.meta.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         IIchan archive search
 // @namespace    https://github.com/WagonOfDoubt/IIchan-archive-search
-// @version      0.1
-// @description  try to take over the world!
-// @icon         http://iichan.hk/favicon.ico
+// @version      0.2
+// @description  Archive viewer userscript for some imageboards
+// @icon         https://iichan.hk/favicon.ico
 // @author       Cirno
-// @updateURL    https://raw.github.com/WagonOfDoubt/IIchan-archive-search/master/IIchan-archive-search.user.js
+// @updateURL    https://raw.github.com/WagonOfDoubt/IIchan-archive-search/master/src/IIchan-archive-search.meta.js
+// @downloadURL  https://raw.github.com/WagonOfDoubt/IIchan-archive-search/master/IIchan-archive-search.user.js
+// @noframes
+// @run-at document-end
 // @include      http://iichan.hk/*/arch/res/*
 // @include      https://iichan.hk/*/arch/res/*
 // @exclude      http://iichan.hk/*/arch/res/*.html
