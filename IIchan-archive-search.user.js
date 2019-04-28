9f506033f772715d1b3d76c0d8d16d58a092f717
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-04-28 13:46:49 +0300

    Change userscript includes (fixes issue #1)

--- IIchan-archive-search.user.js
+++ IIchan-archive-search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IIchan archive search
 // @namespace    https://github.com/WagonOfDoubt/IIchan-archive-search
-// @version      0.2
+// @version      0.3
 // @description  Archive viewer userscript for some imageboards
 // @icon         https://iichan.hk/favicon.ico
 // @author       Cirno
@@ -9,16 +9,9 @@
 // @downloadURL  https://raw.github.com/WagonOfDoubt/IIchan-archive-search/master/IIchan-archive-search.user.js
 // @noframes
 // @run-at document-end
-// @include      http://iichan.hk/*/arch/res/*
-// @include      https://iichan.hk/*/arch/res/*
-// @exclude      http://iichan.hk/*/arch/res/*.html
-// @exclude      https://iichan.hk/*/arch/res/*.html
-// @include      http://410chan.org/*/arch/res/*
-// @include      https://410chan.org/*/arch/res/*
-// @exclude      http://410chan.org/*/arch/res/*.html
-// @exclude      https://410chan.org/*/arch/res/*.html
-// @include      http://nowere.net/*/arch/
-// @include      https://nowere.net/*/arch/
+// @include      /^https?:\/\/410chan\.org\/.+?\/arch\/res\/$/
+// @include      /^https?:\/\/iichan\.hk\/.+?\/arch\/res\/$/
+// @include      /^https?:\/\/nowere\.net\/.+?\/arch\/$/
 // @grant        none
 // ==/UserScript==
 
